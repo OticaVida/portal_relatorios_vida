9853636a23a96441a6e17931c4e15b539acb3c08
Joao Pedro Lacerda <lacerdaj355@gmail.com>
2026-04-27 17:06:47 -0300

    .

--- portal_relatorios_vida/settings.py
+++ portal_relatorios_vida/settings.py
@@ -8,6 +8,11 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path=env_path)
+
+# Teste
+print(f"DEBUG: Carregando banco com usuario: {os.getenv('DB_USER')}")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
